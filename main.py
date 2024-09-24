a0b6dc4f9c86d874fb23bf7a22c7b8d79493d0db
Nithin <nithintpr1572@gmail.com>
2024-09-23 19:52:56 -0500

    Inserting into DB

--- main.py
+++ main.py
@@ -1,8 +1,28 @@
 import streamlit as st  # type: ignore
+import mysql.connector # type: ignore
 
 st.write("## Enter you Details :")
 
 x = st.text_input("Enter your name")
+isClicked = st.button("Enter")
 
-if x != "":
-    st.write(f"hello, {x}!")
+#host user password database
+
+if isClicked and x != "":
+    conn = mysql.connector.connect(
+        host = st.secrets["mysql"]["hostname"],
+        user = st.secrets["mysql"]["username"],
+        password = st.secrets["mysql"]["password"],
+        database = st.secrets["mysql"]["database"]
+    )
+
+    cursor = conn.cursor()
+    q1 = "insert into users values(" + '"' + x + '"' + ");"
+
+    cursor.execute(q1)
+
+    conn.commit() 
+    cursor.close()
+    conn.close()
+
+    st.write(f"Data Inserted!")
