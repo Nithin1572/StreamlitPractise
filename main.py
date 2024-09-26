b25cd6c87a323496b2591e17aab3320e33db48dd
Nithin <nithintpr1572@gmail.com>
2024-09-25 22:00:53 -0500

    Prints the list of records from users table

--- main.py
+++ main.py
@@ -4,25 +4,34 @@ import mysql.connector # type: ignore
 st.write("## Enter you Details :")
 
 x = st.text_input("Enter your name")
-isClicked = st.button("Enter")
 
-#host user password database
+col1, col2 = st.columns(2)
 
-if isClicked and x != "":
-    conn = mysql.connector.connect(
+with col1:
+    insertButton = st.button("Enter Name")
+with col2:
+    showAllButton = st.button("Show all Records")
+
+conn = mysql.connector.connect(
         host = st.secrets["mysql"]["hostname"],
         user = st.secrets["mysql"]["username"],
         password = st.secrets["mysql"]["password"],
         database = st.secrets["mysql"]["database"]
     )
+cursor = conn.cursor()
 
-    cursor = conn.cursor()
-    q1 = "insert into users values(" + '"' + x + '"' + ");"
-
-    cursor.execute(q1)
-
-    conn.commit() 
-    cursor.close()
-    conn.close()
-
+showAllQuery = "select * from users;"
+insertQuery = "insert into users values(" + '"' + x + '"' + ")"
+    
+if insertButton and x != "":
+    cursor.execute(insertQuery)
     st.write(f"Data Inserted!")
+elif showAllButton:
+    cursor.execute(showAllQuery)
+    result = cursor.fetchall()
+    for x in result:
+        st.write(x[0])
+
+conn.commit() 
+cursor.close()
+conn.close()
